25a0f3856aecddf83b553679748ecfbdfdc64a03
Johanna Smith <josmith@pivotal.io>
2018-05-04 16:05:02 -0600

    Improve syslog forwarder application drain reporting
    - strictly controls against drain naming conventions
    
    [#156823653]
    
    Signed-off-by: Mark Alston <malston@pivotal.io>

--- internal/drain/application_space_drains.go
+++ internal/drain/application_space_drains.go
@@ -1,12 +1,14 @@
 package drain
 
 import (
+	"log"
 	"regexp"
+	"strings"
 
 	"code.cloudfoundry.org/cf-drain-cli/internal/cloudcontroller"
 )
 
-var spaceDrainPattern = "space-forwarder-[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}"
+var drainNamePattern = "(space-forwarder|cf-drain)-[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}"
 
 type ApplicationDrainLister struct {
 	appLister    AppLister
@@ -26,39 +28,66 @@ func NewApplicationDrainLister(appLister AppLister, envProvider EnvProvider) App
 	return ApplicationDrainLister{
 		appLister:    appLister,
 		envProvider:  envProvider,
-		appNameRegex: regexp.MustCompile(spaceDrainPattern),
+		appNameRegex: regexp.MustCompile(drainNamePattern),
 	}
 }
 
 func (dl ApplicationDrainLister) Drains(spaceGUID string) ([]Drain, error) {
-	apps, _ := dl.appLister.ListApps(spaceGUID)
-	guids, names := dl.appMetadata(apps)
+	spaceApps, _ := dl.appLister.ListApps(spaceGUID)
+	guids, names, apps := dl.appMetadata(spaceApps)
 
 	var drains []Drain
-	for _, app := range apps {
+	for _, app := range spaceApps {
 		if dl.appNameRegex.MatchString(app.Name) {
-			// Do I need to be public?
 			envs, err := dl.envProvider.EnvVars(app.Guid)
 			if err != nil {
 				return nil, err
 			}
 
-			drains = append(drains, Drain{
-				Apps:        names,
-				AppGuids:    guids,
-				Type:        envs["DRAIN_TYPE"],
-				DrainURL:    envs["DRAIN_URL"],
-				AdapterType: "application",
-			})
+			switch {
+			case strings.Contains(app.Name, "space-forwarder"):
+				drains = append(drains, Drain{
+					Name:        app.Name,
+					Guid:        app.Guid,
+					Apps:        names,
+					AppGuids:    guids,
+					Type:        envs["DRAIN_TYPE"],
+					DrainURL:    envs["DRAIN_URL"],
+					AdapterType: "application",
+				})
+			case strings.Contains(app.Name, "cf-drain"):
+				sourceID, ok := envs["SOURCE_ID"]
+				if !ok {
+					log.Printf("failed to fetch environment variable SOURCE_ID for %s", app.Name)
+					continue
+				}
+				name, ok := apps[sourceID]
+				if !ok {
+					log.Printf("something went very wrong: failed to retrieve app name for %s", sourceID)
+					continue
+				}
+
+				drains = append(drains, Drain{
+					Name:        app.Name,
+					Guid:        app.Guid,
+					Apps:        []string{name},
+					AppGuids:    []string{sourceID},
+					Type:        envs["DRAIN_TYPE"],
+					DrainURL:    envs["SYSLOG_URL"],
+					AdapterType: "application",
+				})
+			}
 		}
 	}
 	return drains, nil
 }
 
-func (dl ApplicationDrainLister) appMetadata(apps []cloudcontroller.App) (guids []string, names []string) {
+func (dl ApplicationDrainLister) appMetadata(apps []cloudcontroller.App) (guids []string, names []string, spaceApps map[string]string) {
+	spaceApps = make(map[string]string)
 	for _, app := range apps {
+		spaceApps[app.Guid] = app.Name
 		guids = append(guids, app.Guid)
 		names = append(names, app.Name)
 	}
-	return guids, names
+	return guids, names, spaceApps
 }
--- internal/drain/application_space_drains_test.go
+++ internal/drain/application_space_drains_test.go
@@ -14,15 +14,23 @@ var _ = Describe("ApplicationSpaceDrains", func() {
 
 	var (
 		appLister   *spyAppLister
-		envProvider *stubEnvProvider
+		envProvider *spyEnvProvider
 		drainLister drain.ApplicationDrainLister
 	)
 
 	BeforeEach(func() {
 		appLister = newSpyAppLister()
-		envProvider = newStubEnvProvider()
+		envProvider = newSpyEnvProvider()
 
+		drainLister = drain.NewApplicationDrainLister(appLister, envProvider)
+	})
+
+	It("returns all application drains", func() {
 		appLister.apps = []cloudcontroller.App{
+			cloudcontroller.App{
+				Name: "cf-drain-00000000-0000-0000-0000-000000000000",
+				Guid: "00000000-0000-0000-0000-000000000000",
+			},
 			cloudcontroller.App{
 				Name: "space-forwarder-11111111-1111-1111-1111-111111111111",
 				Guid: "11111111-1111-1111-1111-111111111111",
@@ -36,40 +44,124 @@ var _ = Describe("ApplicationSpaceDrains", func() {
 				Guid: "33333333-3333-3333-3333-333333333333",
 			},
 		}
-		envProvider.envs = map[string]string{
-			"DRAIN_TYPE": "all",
-			"DRAIN_URL":  "https://the-syslog-drain.com",
+
+		envProvider.envs = map[string]map[string]string{
+			"00000000-0000-0000-0000-000000000000": {
+				"SOURCE_ID":  "22222222-2222-2222-2222-222222222222",
+				"DRAIN_TYPE": "logs",
+				"SYSLOG_URL": "syslog://the-syslog-drain.com",
+			},
+			"11111111-1111-1111-1111-111111111111": {
+				"DRAIN_TYPE": "all",
+				"DRAIN_URL":  "https://the-syslog-drain.com",
+			},
 		}
 
-		drainLister = drain.NewApplicationDrainLister(appLister, envProvider)
-
-	})
-
-	It("returns application space drains", func() {
 		drains, err := drainLister.Drains("space-guid")
 		Expect(err).ToNot(HaveOccurred())
 
-		Expect(drains).To(HaveLen(1))
+		Expect(drains).To(HaveLen(2))
 
-		Expect(drains[0]).To(Equal(
-			drain.Drain{
-				Apps: []string{
-					"space-forwarder-11111111-1111-1111-1111-111111111111",
-					"app-1",
-					"app-2",
+		Expect(drains).To(ConsistOf(
+			[]drain.Drain{
+				drain.Drain{
+					Name: "cf-drain-00000000-0000-0000-0000-000000000000",
+					Guid: "00000000-0000-0000-0000-000000000000",
+					Apps: []string{
+						"app-1",
+					},
+					AppGuids: []string{
+						"22222222-2222-2222-2222-222222222222",
+					},
+					Type:        "logs",
+					DrainURL:    "syslog://the-syslog-drain.com",
+					AdapterType: "application",
 				},
-				AppGuids: []string{
-					"11111111-1111-1111-1111-111111111111",
-					"22222222-2222-2222-2222-222222222222",
-					"33333333-3333-3333-3333-333333333333",
+				drain.Drain{
+					Name: "space-forwarder-11111111-1111-1111-1111-111111111111",
+					Guid: "11111111-1111-1111-1111-111111111111",
+					Apps: []string{
+						"cf-drain-00000000-0000-0000-0000-000000000000",
+						"space-forwarder-11111111-1111-1111-1111-111111111111",
+						"app-1",
+						"app-2",
+					},
+					AppGuids: []string{
+						"00000000-0000-0000-0000-000000000000",
+						"11111111-1111-1111-1111-111111111111",
+						"22222222-2222-2222-2222-222222222222",
+						"33333333-3333-3333-3333-333333333333",
+					},
+					Type:        "all",
+					DrainURL:    "https://the-syslog-drain.com",
+					AdapterType: "application",
 				},
-				Type:        "all",
-				DrainURL:    "https://the-syslog-drain.com",
-				AdapterType: "application",
 			},
 		))
 	})
+
+	Context("when syslog forwarder app does not contain env var SOURCE_ID", func() {
+		It("does not report that drain ", func() {
+			appLister.apps = []cloudcontroller.App{
+				cloudcontroller.App{
+					Name: "cf-drain-00000000-0000-0000-0000-000000000000",
+					Guid: "00000000-0000-0000-0000-000000000000",
+				},
+				cloudcontroller.App{
+					Name: "space-forwarder-11111111-1111-1111-1111-111111111111",
+					Guid: "11111111-1111-1111-1111-111111111111",
+				},
+				cloudcontroller.App{
+					Name: "app-1",
+					Guid: "22222222-2222-2222-2222-222222222222",
+				},
+			}
+
+			envProvider.envs = map[string]map[string]string{
+				"00000000-0000-0000-0000-000000000000": {
+					"DRAIN_TYPE": "logs",
+					"SYSLOG_URL": "syslog://the-syslog-drain.com",
+				},
+				"11111111-1111-1111-1111-111111111111": {
+					"DRAIN_TYPE": "all",
+					"DRAIN_URL":  "https://the-syslog-drain.com",
+				},
+			}
+
+			drains, err := drainLister.Drains("space-guid")
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(drains).To(HaveLen(1))
+
+			Expect(drains).To(ConsistOf(
+				drain.Drain{
+					Name: "space-forwarder-11111111-1111-1111-1111-111111111111",
+					Guid: "11111111-1111-1111-1111-111111111111",
+					Apps: []string{
+						"cf-drain-00000000-0000-0000-0000-000000000000",
+						"space-forwarder-11111111-1111-1111-1111-111111111111",
+						"app-1",
+					},
+					AppGuids: []string{
+						"00000000-0000-0000-0000-000000000000",
+						"11111111-1111-1111-1111-111111111111",
+						"22222222-2222-2222-2222-222222222222",
+					},
+					Type:        "all",
+					DrainURL:    "https://the-syslog-drain.com",
+					AdapterType: "application",
+				},
+			))
+		})
+	})
+
 	It("logs an error when it fails to retrieve environment variables", func() {
+		appLister.apps = []cloudcontroller.App{
+			cloudcontroller.App{
+				Name: "space-forwarder-00000000-0000-0000-0000-000000000000",
+				Guid: "00000000-0000-0000-0000-000000000000",
+			},
+		}
 		e := errors.New("some err")
 		envProvider.err = e
 
@@ -77,6 +169,7 @@ var _ = Describe("ApplicationSpaceDrains", func() {
 
 		Expect(err).To(HaveOccurred())
 	})
+
 })
 
 func newSpyAppLister() *spyAppLister {
@@ -93,15 +186,15 @@ func (s *spyAppLister) ListApps(spaceGuid string) ([]cloudcontroller.App, error)
 	return s.apps, nil
 }
 
-func newStubEnvProvider() *stubEnvProvider {
-	return &stubEnvProvider{}
+func newSpyEnvProvider() *spyEnvProvider {
+	return &spyEnvProvider{}
 }
 
-type stubEnvProvider struct {
-	envs map[string]string
+type spyEnvProvider struct {
+	envs map[string]map[string]string
 	err  error
 }
 
-func (e *stubEnvProvider) EnvVars(appGuid string) (map[string]string, error) {
-	return e.envs, e.err
+func (e *spyEnvProvider) EnvVars(appGuid string) (map[string]string, error) {
+	return e.envs[appGuid], e.err
 }

31914b8cc040fea2183ead8f8d2b9583ce688057
Andrew Poydence <apoydence@pivotal.io>
2018-05-16 12:02:46 -0600

    Adds acceptance tests for 'drains', 'delete-drain', 'bind-drain' and 'drain-space'
    [#157561637]
    
    Signed-off-by: Mark Alston <malston@pivotal.io>

--- acceptance/drain/service_drain.go
+++ acceptance/drain/service_drain.go
@@ -1,10 +1,13 @@
 package drain
 
 import (
+	"fmt"
+	"sync"
 	"time"
 
 	"code.cloudfoundry.org/cf-drain-cli/acceptance"
 	. "code.cloudfoundry.org/cf-drain-cli/acceptance/helpers"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,33 +23,62 @@ var _ = Describe("ServiceDrain", func() {
 		logWriterAppName2 string
 		interrupt         chan struct{}
 		logs              *Session
+		drains            *Session
+		drainsRegex       = `App                              Drain                                          Type      URL                        AdapterType
+LOG-EMITTER-1--[0-9a-f]{16}  cf-drain-[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}  Logs      syslog://\d+[.]\d+[.]\d+[.]\d+:\d+  service`
 	)
 
 	BeforeEach(func() {
 		interrupt = make(chan struct{}, 1)
 
-		listenerAppName = PushSyslogServer()
-		logWriterAppName1 = PushLogWriter()
-		logWriterAppName2 = PushLogWriter()
+		var wg sync.WaitGroup
+		defer wg.Wait()
+
+		wg.Add(3)
+		go func() {
+			defer wg.Done()
+			listenerAppName = PushSyslogServer()
+		}()
+		go func() {
+			defer wg.Done()
+			logWriterAppName1 = PushLogWriter()
+		}()
+		go func() {
+			defer wg.Done()
+			logWriterAppName2 = PushLogWriter()
+		}()
 	})
 
 	AfterEach(func() {
-		logs.Kill()
+		if logs != nil {
+			logs.Kill()
+		}
+		if drains != nil {
+			drains.Kill()
+		}
+
 		close(interrupt)
 
-		CF("delete", logWriterAppName1, "-f", "-r")
-		CF("delete", logWriterAppName2, "-f", "-r")
-		CF("delete", listenerAppName, "-f", "-r")
+		var wg sync.WaitGroup
+		defer wg.Wait()
+		deleteApp := func(appName string) {
+			defer wg.Done()
+			CF("delete", appName, "-f", "-r")
+		}
+
+		wg.Add(3)
+		go deleteApp(logWriterAppName1)
+		go deleteApp(logWriterAppName2)
+		go deleteApp(listenerAppName)
 	})
 
 	It("drains an app's logs to syslog endpoint", func() {
 		syslogDrainURL := "syslog://" + SyslogDrainAddress(listenerAppName)
 
-		CF("drain",
+		CF(
+			"drain",
 			logWriterAppName1,
 			syslogDrainURL,
-			"--adapter-type",
-			"service",
 		)
 
 		randomMessage1 := generator.PrefixedRandomName("RANDOM-MESSAGE-A", "LOG")
@@ -59,5 +91,99 @@ var _ = Describe("ServiceDrain", func() {
 
 		Eventually(logs, acceptance.Config().DefaultTimeout+3*time.Minute).Should(Say(randomMessage1))
 		Consistently(logs, 10).ShouldNot(Say(randomMessage2))
+	})
+
+	It("binds an app to a syslog endpoint", func() {
+		syslogDrainURL := "syslog://" + SyslogDrainAddress(listenerAppName)
+		drainName := fmt.Sprintf("some-drain-%d", time.Now().UnixNano())
+
+		CF(
+			"drain",
+			logWriterAppName1,
+			syslogDrainURL,
+			"--drain-name", drainName,
+		)
+
+		CF(
+			"bind-drain",
+			logWriterAppName2,
+			drainName,
+		)
+
+		randomMessage1 := generator.PrefixedRandomName("RANDOM-MESSAGE-A", "LOG")
+		randomMessage2 := generator.PrefixedRandomName("RANDOM-MESSAGE-B", "LOG")
+
+		logs = LogsFollow(listenerAppName)
+
+		go WriteToLogsApp(interrupt, randomMessage1, logWriterAppName1)
+		go WriteToLogsApp(interrupt, randomMessage2, logWriterAppName2)
+
+		Eventually(logs, acceptance.Config().DefaultTimeout+3*time.Minute).Should(Say(randomMessage1))
+		Eventually(logs, acceptance.Config().DefaultTimeout+3*time.Minute).Should(Say(randomMessage2))
+	})
+
+	It("drains all apps in space to a syslog endpoint", func() {
+		syslogDrainURL := "syslog://" + SyslogDrainAddress(listenerAppName)
+		drainName := fmt.Sprintf("some-drain-%d", time.Now().UnixNano())
+
+		CFWithTimeout(
+			1*time.Minute,
+			"drain-space",
+			"--drain-url", syslogDrainURL,
+			"--drain-name", drainName,
+			"--username", acceptance.Config().CFAdminUser,
+			"--password", acceptance.Config().CFAdminPassword,
+			"--force",
+		)
+
+		defer CF("delete", "space-drain", "-f", "-r")
+
+		randomMessage1 := generator.PrefixedRandomName("RANDOM-MESSAGE-A", "LOG")
+		randomMessage2 := generator.PrefixedRandomName("RANDOM-MESSAGE-B", "LOG")
+
+		logs = LogsFollow(listenerAppName)
+
+		go WriteToLogsApp(interrupt, randomMessage1, logWriterAppName1)
+		go WriteToLogsApp(interrupt, randomMessage2, logWriterAppName2)
+
+		Eventually(logs, acceptance.Config().DefaultTimeout+3*time.Minute).Should(Say(randomMessage1))
+		Eventually(logs, acceptance.Config().DefaultTimeout+3*time.Minute).Should(Say(randomMessage2))
+	})
+
+	It("lists the all the drains", func() {
+		syslogDrainURL := "syslog://" + SyslogDrainAddress(listenerAppName)
+
+		CF(
+			"drain",
+			logWriterAppName1,
+			syslogDrainURL,
+		)
+
+		drains = cf.Cf("drains")
+		Eventually(drains).Should(Say(drainsRegex))
+	})
+
+	It("deletes the drain", func() {
+		syslogDrainURL := "syslog://" + SyslogDrainAddress(listenerAppName)
+
+		CF(
+			"drain",
+			logWriterAppName1,
+			syslogDrainURL,
+			"--drain-name",
+			"cf-drain-8075db89-6080-4b14-93e5-10d69f24d7e1",
+		)
+
+		drains = cf.Cf("drains")
+		Eventually(drains).Should(Say(drainsRegex))
+
+		CF(
+			"delete-drain",
+			"cf-drain-8075db89-6080-4b14-93e5-10d69f24d7e1",
+			"--force", // Skip confirmation
+		)
+
+		drains = cf.Cf("drains")
+		Consistently(drains, 10).ShouldNot(Say("cf-drain-[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}"))
 	})
 })

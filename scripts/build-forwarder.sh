b3c1114cf9c03b76cf3008f64bf8c0a9378103d2
Andrew Poydence <apoydence@pivotal.io>
2018-07-18 10:39:08 -0600

    Fix build-forwarder.sh script
    The script was tailored for linux machines. Enable it to work with osx machines.
    
    [#158959743]

--- scripts/build-forwarder.sh
+++ scripts/build-forwarder.sh
@@ -3,14 +3,27 @@
 git_root=$(git rev-parse --show-toplevel)
 
 pushd $git_root
-    GOOS=linux go get -u github.com/apoydence/cf-space-security/cmd/...
+    GOOS=linux go get -u -d github.com/apoydence/cf-space-security/cmd/...
+
+    pushd $GOPATH/src/github.com/apoydence/cf-space-security/cmd/proxy
+        GOOS=linux go get -d ./...
+        GOOS=linux go build
+    popd
 
     pushd cmd/group-manager
-        GOOS=linux go build
-    popd
-    pushd cmd/syslog-forwarder
+        GOOS=linux go get -d ./...
         GOOS=linux go build
     popd
 
-    zip -j forwarder.zip cmd/group-manager/group-manager cmd/syslog-forwarder/syslog-forwarder $GOPATH/bin/proxy scripts/run.sh
+    pushd cmd/syslog-forwarder
+        GOOS=linux go get -d ./...
+        GOOS=linux go build
+    popd
+
+    zip \
+        -j forwarder.zip \
+        cmd/group-manager/group-manager \
+        cmd/syslog-forwarder/syslog-forwarder \
+        $GOPATH/src/github.com/apoydence/cf-space-security/cmd/proxy/proxy \
+        scripts/run.sh
 popd
